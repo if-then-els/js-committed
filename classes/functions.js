fa1a87746bcf99e744564cc06737a62e50d64a15
if-then-els <briankanambo308@gmail.com>
2024-09-03 21:19:57 +0300

    try it

---
+++ classes/functions.js
@@ -0,0 +1,7 @@
+//write a function that calculates the age
+//of astudent by differenciating birth year and current
+function AgeStudent(Birthyear, Currentyear) {
+  let age = Currentyear - Birthyear;
+  console.log(age);
+}
+AgeStudent(1920, 2024);
